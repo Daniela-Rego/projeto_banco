7f9a299825e573ef2b1c49349f86303d755596c3
João (Pster) Freitas <joaopsterdev@gmail.com>
2024-03-07 19:40:19 -0300

    fixed: jest running

--- src/entities/conta.spec.ts
+++ src/entities/conta.spec.ts
@@ -1,38 +1,42 @@
-import { Conta } from './conta' ;
-describe("teste conta", () => {
+import { Conta } from './conta';
+describe('teste conta', () => {
+  it('criar uma conta', () => {
+    const novaConta = {
+      cliente: 'Daniela',
+      idade: 20,
+      numero_conta: 1,
+      saldo: 1000,
+    };
+    const contaBanco = new Conta(novaConta);
+    expect(contaBanco).toBeInstanceOf(Conta);
+    expect(contaBanco.cliente).toBe('Daniela');
+  });
 
-    it("criar uma conta", () => {
-        const novaConta ={
-            cliente: "Daniela",
-            idade:20,
-            numero_conta:1
-        }
-        const contaBanco = new Conta(novaConta);
-        expect(contaBanco).toBeInstanceOf(Conta);
-        expect(contaBanco.cliente).toBe("Daniela");
-    });
-   
-   it("Cliente deve ter idade menor que 18 anos", () =>{
-        const menorIdade ={
-            cliente: "Daniela",
-            idade:10,
-            numero_conta:1
-            
-        }
-        //o toThrow valida se a função deu erro por isso nao consegue ver a variavel instanciada
-        expect(()=>{ new Conta(menorIdade)}).toThrow(Error);
-    })
-    it("valida se o numero da conta não é zero",()=>{
-        const contaOk ={
-            cliente: "Daniela",
-            idade:18,
-            numero_conta:0
-        }
-        
-       
-        expect(()=>{ return new Conta(contaOk)}).toThrow();
-    });
-   /* it("se o numero da conta for zero derá um erro",()=>{
+  it('Cliente deve ter idade menor que 18 anos', () => {
+    const menorIdade = {
+      cliente: 'Daniela',
+      idade: 10,
+      numero_conta: 1,
+      saldo: 1000,
+    };
+    //o toThrow valida se a função deu erro por isso nao consegue ver a variavel instanciada
+    expect(() => {
+      new Conta(menorIdade);
+    }).toThrow(Error);
+  });
+  it('valida se o numero da conta não é zero', () => {
+    const contaOk = {
+      cliente: 'Daniela',
+      idade: 18,
+      numero_conta: 0,
+      saldo: 1000,
+    };
+
+    expect(() => {
+      return new Conta(contaOk);
+    }).toThrow();
+  });
+  /* it("se o numero da conta for zero derá um erro",()=>{
         const contaOk ={
             cliente: "Daniela",
             idade:18,
@@ -44,5 +48,4 @@ describe("teste conta", () => {
 
       
     });*/
-
 });
--- src/services/create-conta.spec.ts
+++ src/services/create-conta.spec.ts
@@ -1,57 +1,53 @@
-import { Conta } from "../entities/conta";
-import { ContaRepositoryInMemory } from "../repository/in-memory/inMemoryContaRepository";
-import { CreateConta } from "./create-conta"
+import { Conta } from '../entities/conta';
+import { ContaRepositoryInMemory } from '../repository/in-memory/inMemoryContaRepository';
+import { CreateConta } from './create-conta';
 
-describe("testando o create conta",()=>{
-    
-    it("testando o execute",()=>{
-        const bodyTest = {
-            cliente: "Daniela",
-            idade:20,
-            numero_conta:1,
-            saldo:1000
-        }
-        const repositoryInMerory = new ContaRepositoryInMemory();
-        const createConta = new CreateConta(repositoryInMerory);
-       const result = createConta.execute(bodyTest);
+describe('testando o create conta', () => {
+  it('testando o execute', () => {
+    const bodyTest = {
+      cliente: 'Daniela',
+      idade: 20,
+      numero_conta: 1,
+      saldo: 1000,
+    };
+    const repositoryInMerory = new ContaRepositoryInMemory();
+    const createConta = new CreateConta(repositoryInMerory);
+    const result = createConta.execute(bodyTest);
 
-       console.log("result test",result)
-        expect(result).resolves.toBeInstanceOf(Conta)
-    })
-   it("testando o execute error",()=>{
-        const bodyTest = {
-            cliente: "Daniela",
-            idade:0,
-            numero_conta:1,
-            saldo:1000
-        }
-        const repositoryInMerory = new ContaRepositoryInMemory();
-        const createConta = new CreateConta(repositoryInMerory);
-       const result =  createConta.execute(bodyTest);
-       console.log("result test",result)
-        expect(result).rejects.toThrow(Error)
-    });
+    console.log('result test', result);
+    expect(result).resolves.toBeInstanceOf(Conta);
+  });
 
-    it("Nao pode criar pq a conta ja existe",async ()=>{
-        //Duvida: depois de colocar um await no createConta e outro sem, funcionou, pq ?
-        //o ".rejects" ja espera o resultado da função.
-        const bodyTest = {
-            cliente: "Daniela",
-            idade:20,
-            numero_conta:1,
-            saldo:1000
-        }
-        const repositoryInMerory = new ContaRepositoryInMemory();
-        const createConta = new CreateConta(repositoryInMerory);
-       await createConta.execute(bodyTest);
+  it('testando o execute error', () => {
+    const bodyTest = {
+      cliente: 'Daniela',
+      idade: 0,
+      numero_conta: 1,
+      saldo: 1000,
+    };
+    const repositoryInMerory = new ContaRepositoryInMemory();
+    const createConta = new CreateConta(repositoryInMerory);
+    const result = createConta.execute(bodyTest);
+    console.log('result test', result);
+    expect(result).rejects.toThrow(Error);
+  });
 
-       const result =  createConta.execute(bodyTest)
+  it('Nao pode criar pq a conta ja existe', async () => {
+    //Duvida: depois de colocar um await no createConta e outro sem, funcionou, pq ?
+    //o ".rejects" ja espera o resultado da função.
+    const bodyTest = {
+      cliente: 'Daniela',
+      idade: 20,
+      numero_conta: 1,
+      saldo: 1000,
+    };
+    const repositoryInMerory = new ContaRepositoryInMemory();
+    const createConta = new CreateConta(repositoryInMerory);
+    await createConta.execute(bodyTest);
 
-       console.log("result test",result);
-       expect(result).rejects.toThrow(Error);
-    })
-    
-    
-   
-    
-})
+    const result = createConta.execute(bodyTest);
+
+    console.log('result test', result);
+    expect(result).rejects.toThrow(Error);
+  });
+});
